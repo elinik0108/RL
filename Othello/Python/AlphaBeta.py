5a06800b8c13dc91b0d39043ccddfdd03581d96a
Ola Ringdahl <ringdahl@cs.umu.se>
2021-10-18 18:08:18 +0200

    Updated AlphaBeta.py

--- Othello/Python/AlphaBeta.py
+++ Othello/Python/AlphaBeta.py
@@ -5,7 +5,7 @@ from OthelloAction import OthelloAction
 
 class AlphaBeta(OthelloAlgorithm):
     """
-    This is where you implement the alpha-beta-algorithm. 
+    This is where you implement the alpha-beta algorithm. 
 	See OthelloAlgorithm for details
 	
     Author:
@@ -13,14 +13,14 @@ class AlphaBeta(OthelloAlgorithm):
 	DefaultDepth = 5 
 
     def __init__(self, othello_evaluator=CountingEvaluator(), depth=DefaultDepth):
-        self.evaluator = othello_evaluator # change to your own evaluator
-        self.search_depth = depth # you probably don't want to have a hard coded depth
+        self.evaluator = othello_evaluator 
+        self.search_depth = depth 
 
     def set_evaluator(self, othello_evaluator, ):
-        self.evaluator = othello_evaluator 
+        self.evaluator = othello_evaluator # change to your own evaluator
 
     def set_search_depth(self, depth):
-        self.search_depth = depth 
+        self.search_depth = depth # use iterative deepening search to decide depth
 
     def evaluate(self, othello_position):
         # TODO: implement the alpha-beta algorithm
