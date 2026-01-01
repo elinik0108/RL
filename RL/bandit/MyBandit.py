2b4b2ba9f49a54faf3d35edf75df8d7e01d2bf12
cmdking <your-email@example.com>
2026-01-01 21:15:55 +0100

    Added an optimized verison of MAB based on environment changes

--- RL/bandit/MyBandit.py
+++ RL/bandit/MyBandit.py
@@ -5,18 +5,21 @@ class Bandit:
     """
     Generic epsilon-greedy bandit that you need to improve
     """
-    def __init__(self, arms, epsilon=0.1):
+    def __init__(self, arms, epsilon=0.1, alpha= 0.1):
         """
         Initiates the bandits
 
         :param arms: List of arms
         :param epsilon: Epsilon value for random exploration
+        :alpha : alpha value for random exploration
         """
         self.arms = arms
         self.epsilon = epsilon
+        self.alpha = alpha
+
         self.frequencies = [0] * len(arms)
-        self.sums = [0] * len(arms)
-        self.expected_values = [0] * len(arms)
+        self.sums = [0.0] * len(arms)
+        self.expected_values = [0.0] * len(arms)
 
     def run(self):
         """
@@ -24,11 +27,26 @@ class Bandit:
 
         :return: Returns the arm the bandit recommends pulling
         """
-        if min(self.frequencies) == 0:
-            return self.arms[self.frequencies.index(min(self.frequencies))]
+
+        # checking that each arm is tried at least once
+        for i, freq in enumerate(self.frequencies):
+            if freq == 0:
+                return self.arms[i]
+
+        # explorations
         if random.random() < self.epsilon:
-            return self.arms[random.randint(0, len(self.arms) - 1)]
-        return self.arms[self.expected_values.index(max(self.expected_values))]
+            return random.choice(self.arms)
+
+        max_q = max(self.expected_values)
+
+        ## create a list of all positions where value is equal to max
+        best_index = []
+        for i, q in enumerate(self.expected_values):
+            if q == max_q:
+                best_index.append(i)
+
+        # return list of arms with randomly selected value from best_index
+        return self.arms[random.choice(best_index)]
 
     def give_feedback(self, arm, reward):
         """
@@ -38,9 +56,12 @@ class Bandit:
         :param reward: The reward that was generated
         """
         arm_index = self.arms.index(arm)
-        sum = self.sums[arm_index] + reward
-        self.sums[arm_index] = sum
-        frequency = self.frequencies[arm_index] + 1
-        self.frequencies[arm_index] = frequency
-        expected_value = sum / frequency
-        self.expected_values[arm_index] = expected_value
+
+        # update frequency
+        self.frequencies[arm_index] += 1
+
+        self.sums[arm_index] += reward
+        # unlike the standard epsilon greedy MAB that implicitly the reward destribution
+        # of each arm does not change overtime. Here we update reward based on environmnet changes that
+        # happens in "reward - self.expected_values[arm_index]"
+        self.expected_values[arm_index] += self.alpha * (reward - self.expected_values[arm_index])
